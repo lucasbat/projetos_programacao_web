ad9726bd95b1b9b20c365da6d54cb7eca685a6af
gurucriacoes <lucaspro2@hotmail.com>
2021-12-27 19:25:49 -0300

    update

--- JAVASCRIPT/weather_tempo_clima_javascript/script.js
+++ JAVASCRIPT/weather_tempo_clima_javascript/script.js
@@ -5,12 +5,9 @@ document.querySelector('.busca').addEventListener('submit', async (event) =>{
   
     if(input !== ''){
         showWarning('Carregando...');
-        
         let url = `http://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=6d74a2ec006f14f1fe31ea2d92435e13&units=metric&lang=pt_br`;
-        
         let results = await fetch(url);
         let json = await results.json();
-       
         if(json.cod === 200) {
             showInfo({
                 name: json.name,
@@ -27,15 +24,11 @@ document.querySelector('.busca').addEventListener('submit', async (event) =>{
 })
 function showInfo(json) {
     showWarning('');
-
     document.querySelector('.resultado').style.display = 'block';
-
     document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
     document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`;
     document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`;
-
     document.querySelector('.tempo img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
-
     document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`;
 }
 
